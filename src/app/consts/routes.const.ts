5ba6f02f23f3c0788628e08891ad61239e46cf11
Yun Ji Liao <60011714+yuliaaPoly@users.noreply.github.com>
2024-03-07 00:36:42 -0500

    clear

--- src/app/consts/routes.const.ts
+++
@@ -1,5 +0,0 @@
-export const BASE = '';
-export const FEED = 'feed';
-export const CREATE = 'create';
-// export const TEMPORARY_WIP = 'wip';
-// export const UNDER_CONSTRUCTION = 'under-development';
--- src/app/consts/useful-constants.ts
+++
@@ -1,4 +0,0 @@
-// for WIP page
-export const MAX_LENGTH_MESSAGE = 3000;
-export const DESIRED_LENGTH_PHONE = 10;
-export const MIN_LENGTH_MESSAGE = 20;
